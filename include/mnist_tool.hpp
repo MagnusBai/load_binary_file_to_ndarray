01e497715e92b571e840f6ccf8bef0b825830f17
MagnusBai <mingran.muc@gmail.com>
2018-01-04 07:52:37 +0800

    ADD-FEATURE: load image file

--- include/mnist_tool.hpp
+++ include/mnist_tool.hpp
@@ -6,10 +6,15 @@
 #include <iostream>
 #include <fstream>
 #include <assert.h>
+#include <ios>
 
 #ifndef IN
 #define IN
 #define OUT
+#endif
+
+#ifndef __ERRORHANDLE__
+#define __ERRORHANDLE__
 #endif
 
 using std::shared_ptr;
@@ -22,48 +27,63 @@ uint32_t swap_endian(uint32_t val) {
 bool createTrainLabelsIdx1Ubyte(const char* IN path, shared_ptr<int>& OUT ptr_mat, uint32_t& OUT H, uint32_t& OUT W);
 
 bool createTrainLabelsIdx1Ubyte(const char* IN path, shared_ptr<int>& OUT ptr_mat, uint32_t& OUT H, uint32_t& OUT W) {
-  // FILE * p_file;
-  // p_file = fopen (path , "rb");
-  // if(p_file == NULL) { return false; }
-  // {
-  //   char magic_number[4];
-  //   if( fgets(magic_number, 4, p_file)==NULL ) { return false; }
-  //   #ifdef DEBUG 
-  //   std::cout << "magic number: " << swap_endian(*((uint32_t*)magic_number)) << std::endl;
-  //   #endif
-  // }
-  // {
-  //   char number_of_items[4];
-  //   if( fgets(number_of_items, 4, p_file)==NULL ) { return false; }
-  //   W=1; H=*((uint32_t*)number_of_items);
-  //   #ifdef DEBUG
-  //   std::cout << "number of items: " << H << std::endl;
-  //   std::cout << "number of items: " << swap_endian(H) << std::endl;
-  //   #endif
-  // }
-  // fclose (p_file);
   std::ifstream label_file(path, std::ios::in | std::ios::binary);
+  {
+    __ERRORHANDLE__ if(!label_file.good()) return false;
+  }
   {
     uint32_t magic;
     label_file.read(reinterpret_cast<char*>(&magic), 4);
+    __ERRORHANDLE__ if(!label_file.good()) return false;
     magic = swap_endian(magic);
-    if(magic!=2049) return false;
+    __ERRORHANDLE__ if(magic!=2049) return false;
   }
   {
     uint32_t num_labels;
     label_file.read(reinterpret_cast<char*>(&num_labels), 4);
+    __ERRORHANDLE__ if(!label_file.good()) return false;
     num_labels = swap_endian(num_labels);
     H=num_labels; W=1;
-    if(num_labels!=60000) return false;
+    __ERRORHANDLE__ if(num_labels!=60000) return false;
   }
   ptr_mat = shared_ptr<int>(new int [W*H]);
   {
     char label;
     for (int item_id = 0; item_id < H; ++item_id) {
       label_file.read(&label, 1);
+      __ERRORHANDLE__ if(!label_file.good()) return false;
       ptr_mat.get()[item_id]=(int)label;
     }
   }
+  return true;
+}
+
+bool createTrainImagesIdx3Ubyte(const char* IN path, shared_ptr<int>& OUT ptr_mat, uint32_t& OUT N, uint32_t& OUT H, uint32_t& OUT W) {
+  std::ifstream image_file(path, std::ios::in | std::ios::binary);
+
+  uint32_t magic, num_items, rows, cols;
+
+  image_file.read(reinterpret_cast<char*>(&magic), 4);
+  magic = swap_endian(magic);
+
+  image_file.read(reinterpret_cast<char*>(&num_items), 4);
+  num_items = swap_endian(num_items);
+
+  image_file.read(reinterpret_cast<char*>(&rows), 4);
+  rows = swap_endian(rows);
+  image_file.read(reinterpret_cast<char*>(&cols), 4);
+  cols = swap_endian(cols);
+
+  H=rows; W=cols; N=num_items;
+  ptr_mat = shared_ptr<int>(new int [N*H*W]);
+  char* pixels = new char[rows * cols];
+  for (int item_id = 0; item_id < num_items; ++item_id) {
+    image_file.read(pixels, rows * cols);
+    for(int i=0; i<H*W; ++i) {
+      ptr_mat.get()[item_id*H*W+i] = (int)(pixels[i]);
+    }
+  }
+
   return true;
 }
 
--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@ using std::shared_ptr;
 
 int main() {
   shared_ptr<int> ptr_train_label; uint32_t w_train_label, h_train_label;
-  createTrainLabelsIdx1Ubyte("data/train-labels-idx1-ubyte", ptr_train_label, h_train_label, w_train_label);
+  if(!
+    createTrainLabelsIdx1Ubyte("data/train-labels-idx1-ubyte", ptr_train_label, h_train_label, w_train_label)
+  ) {
+    std::cout << "loading train-labels-idx1-ubyte failed\n";
+  }
+
+	shared_ptr<int> ptr_train_images; uint32_t w_train_im, h_train_im, n_train_im;
+  createTrainImagesIdx3Ubyte("data/train-images-idx3-ubyte", ptr_train_images, n_train_im, h_train_im, w_train_im);
   return 0;
 }
